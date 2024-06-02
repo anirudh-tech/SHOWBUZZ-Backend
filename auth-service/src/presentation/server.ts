edcadf2c25c39f94e83eec93cb4762b26dc3f92a
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-02 20:42:14 +0530

    auth service log added

--- auth-service/src/presentation/server.ts
+++ auth-service/src/presentation/server.ts
@@ -12,6 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
+app.use((req, res, next) => {
+  console.log('hellooo')
+  next();
+})
+
 app.use("/", authRoutes(dependencies));
 
 app.use((
