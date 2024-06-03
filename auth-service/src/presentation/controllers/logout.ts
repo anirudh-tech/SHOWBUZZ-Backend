b8c499b21b401de0998ded2d584afb14c3365694
Anirudh-tech <anirudhjagath2002@gmail.com>
2024-06-03 12:56:36 +0530

    fix: cookies fixed

--- auth-service/src/presentation/controllers/logout.ts
+++ auth-service/src/presentation/controllers/logout.ts
@@ -5,7 +5,11 @@ export const logoutController = (dependencies: IDependencies) => {
 
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
-            res.clearCookie("user_jwt");
+            res.clearCookie("user_jwt",{
+                httpOnly: true,
+                secure: true,
+                sameSite: "none"
+            });
             res.status(200).json({message:"Logged out"});
         } catch (error : any) {
             next(error)
