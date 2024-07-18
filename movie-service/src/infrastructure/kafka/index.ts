e4d9139dca7394d5eea8b5e7320071d6f447c4d4
anirudh-tech <anirudhjagath2002@gmail.com>
2024-07-18 15:16:32 +0530

    fix:kafka port changed

--- movie-service/src/infrastructure/kafka/index.ts
+++ movie-service/src/infrastructure/kafka/index.ts
@@ -1,9 +1,23 @@
 import { Kafka, Producer, Consumer } from "kafkajs";
 
-export const kafka = new Kafka({
-    clientId: "movie-service",
-    brokers:["34.93.145.38:29092"],
-})
+// export const kafka = new Kafka({
+//     clientId: "movie-service",
+//     brokers:["34.93.145.38:29092"],
+// })
+
+const kafka = new Kafka({
+    clientId: 'movie-service',
+    brokers: ['pkc-4j8dq.southeastasia.azure.confluent.cloud:9092'],
+    ssl: true,
+    sasl: {
+        mechanism: 'plain',
+        username: 'AH3AIXDBMRITWY2S',
+        password: 'PTTwXBptxZjyOa3DLtmSIjgC3mg8AZG8o1MB0pShQvbNX7bTC07O8HcgLAi+sqUj',
+    },
+    connectionTimeout: 30000,
+    authenticationTimeout: 30000,
+
+});
 
 export const producer: Producer = kafka.producer();
 export const consumer: Consumer = kafka.consumer({
--- user-service/src/infrastructure/kafka/index.ts
+++ user-service/src/infrastructure/kafka/index.ts
@@ -1,10 +1,23 @@
 import { Kafka, Producer, Consumer } from "kafkajs";
 
-export const kafka = new Kafka({
-    clientId: "user-service",
-    brokers:["34.93.145.38:29092"],
-})
+// export const kafka = new Kafka({
+//     clientId: "user-service",
+//     brokers:["34.93.145.38:29092"],
+// })
 
+const kafka = new Kafka({
+    clientId: 'user-service',
+    brokers: ['pkc-4j8dq.southeastasia.azure.confluent.cloud:9092'],
+    ssl: true,
+    sasl: {
+      mechanism: 'plain',
+      username: 'AH3AIXDBMRITWY2S',
+      password:'PTTwXBptxZjyOa3DLtmSIjgC3mg8AZG8o1MB0pShQvbNX7bTC07O8HcgLAi+sqUj',
+    },
+    connectionTimeout: 30000, 
+      authenticationTimeout: 30000, 
+  
+  });
 export const producer: Producer = kafka.producer();
 export const consumer: Consumer = kafka.consumer({
     groupId: "user-service-kafka-group"
